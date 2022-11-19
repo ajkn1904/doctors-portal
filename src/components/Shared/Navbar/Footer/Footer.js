a3e09aba653bb56f3a03e2b2a7ad4f0095dd6090
ajkn1904 <ajknishat@gmail.com>
2022-11-19 08:08:03 +0600

    update

--- src/components/Shared/Navbar/Footer/Footer.js
+++ src/components/Shared/Navbar/Footer/Footer.js
@@ -5,29 +5,28 @@ import footerBg from '../../../../assets/images/footer.png'
 const Footer = () => {
     return (
         <footer className="p-10" style={{background: `url(${footerBg})`, backgroundSize: 'cover'}}>
-            <div className='footer'>
+            <div className='footer  ml-24'>
                 <div>
-                    <span className="footer-title">Services</span>
-                    <Link to='/' className="link link-hover">Branding</Link>
-                    <Link to='/' className="link link-hover">Design</Link>
-                    <Link to='/' className="link link-hover">Marketing</Link>
-                    <Link to='/' className="link link-hover">Advertisement</Link>
+                    <span className="footer-title">SERVICES</span>
+                    <Link to='/' className="link link-hover">Emergency Checkup</Link>
+                    <Link to='/' className="link link-hover">Monthly Checkup</Link>
+                    <Link to='/' className="link link-hover">Weekly Checkup</Link>
+                    <Link to='/' className="link link-hover">Deep Checkup</Link>
                 </div>
                 <div>
-                    <span className="footer-title">Company</span>
-                    <Link to='/' className="link link-hover">About us</Link>
-                    <Link to='/' className="link link-hover">Contact</Link>
-                    <Link to='/' className="link link-hover">Jobs</Link>
-                    <Link to='/' className="link link-hover">Press kit</Link>
+                    <span className="footer-title">ORAL HEALTH</span>
+                    <Link to='/' className="link link-hover">Fluoride Treatment</Link>
+                    <Link to='/' className="link link-hover">Cavity Filling</Link>
+                    <Link to='/' className="link link-hover">Teath Whitening</Link>
                 </div>
                 <div>
-                    <span className="footer-title">Legal</span>
-                    <Link to='/' className="link link-hover">Terms of use</Link>
-                    <Link to='/' className="link link-hover">Privacy policy</Link>
-                    <Link to='/' className="link link-hover">Cookie policy</Link>
+                    <span className="footer-title">OUR ADDRESS</span>
+                    <Link to='/' className="link link-hover">New York - 101010 Hudson</Link>
                 </div>
             </div>
             <div className='text-center mt-16'>
+                <Link to='/' className="link link-hover">Terms of use</Link> <> || </> 
+                <Link to='/' className="link link-hover">Privacy policy</Link>
                 <p>Copyright © 2022 - All right reserved by Anika Jumana Khanam</p>
             </div>
         </footer>
